6b8b785a45b1ca850b58ecb8e668ae07d7ceee5d
Kenney Curlee <bassetthnd824@gmail.com>
2025-09-05 21:35:51 -0500

    Initial commit

---
+++ apps/daily-quiz-monorepo/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@daily-quiz-monorepo/daily-quiz-monorepo',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/daily-quiz-monorepo',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
